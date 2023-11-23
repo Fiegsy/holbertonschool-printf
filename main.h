19b7d8ea8e6195f0e68666481a7f62631bf682e2
benjaminparentt <p.benjamin620@icloud.com>
2023-11-23 02:29:33 -0800

    change putchar name

--- main.h
+++ main.h
@@ -5,20 +5,20 @@
 #include <stdlib.h>
 
 /**
- * struct specifier - Structure of the format
+ * struct specifiers - Structure of the format
  * @modifiers: Structure of the format
  * @f: the procedure associated
  *
  */
 
-typedef struct specifier
+typedef struct specifiers
 {
-	char specifier;
+	char specifiers;
 	int (*f)(va_list);
 } spec_t;
 
 int _printf(const char *format, ...);
-int _putchar(char c);
+int _myputchar(char c);
 int get_func(char conv, va_list args);
 
 int print_mod(va_list args);

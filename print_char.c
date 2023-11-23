7e571349baacdd5b821760d0c71d51adfd950611
Fiegsy <yassine.daoui031@gmail.com>
2023-11-23 02:01:22 -0800

    Added char function file

---
+++ print_char.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_char - function to print a char
+ *
+ * @args: argument
+ * Return: Success
+ */
+int print_char(va_list  args)
+{
+	char c = va_arg(args, int);
+
+	_myputchar(c);
+
+	return (1);
+}

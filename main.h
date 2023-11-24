a08f1376ffdfdf61272e5f1aa10be65c29eb2816
Fiegsy <yassine.daoui031@gmail.com>
2023-11-24 05:43:32 -0800

    Added missing header to main

--- main.h
+++ main.h
@@ -5,15 +5,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int _printf(const char *format, ...);
-int _myputchar(char c);
-int print_mod(va_list args);
-int print_string(va_list args);
-int print_char(va_list args);
-
-
-
-
 /**
  * struct specifiers - Structure of the format
  * @modifiers: Structure of the format
@@ -21,16 +12,19 @@ int print_char(va_list args);
  *
  */
 
-
-
 typedef struct specifiers
 {
         char specifiers;
         int (*f)(va_list);
 } spec_t;
 
+int _printf(const char *format, ...);
+int _myputchar(char c);
 int get_func(char conv, va_list args);
+
+int print_mod(va_list args);
+int print_string(va_list args);
+int print_char(va_list args);
 int print_num(va_list args);
 
-
-#endif 
+#endif /* MAIN_H */ 

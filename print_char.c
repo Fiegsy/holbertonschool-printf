179a85fbcb85349601e260a8e3cb6df73f162529
Fiegsy <yassine.daoui031@gmail.com>
2023-11-27 01:43:55 -0800

    betty

--- print_char.c
+++ print_char.c
@@ -2,16 +2,14 @@
 #include <unistd.h>
 /**
  * print_char - function to print char
- *
  * @args: argument
  * Return: Success
  */
 int print_char(va_list  args)
 {
-        char c = va_arg(args, int);
+	char c = va_arg(args, int);
 
-        _myputchar(c);
+	_myputchar(c);
 
-        return (1);
+	return (1);
 }
-
